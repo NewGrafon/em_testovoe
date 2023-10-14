28a464939e0be84491d42ae1521cabe19521a4d9
NewGrafon <ultrafig73@gmail.com>
2023-10-15 00:06:56 +0400

    edit bootstrap config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
+import * as process from 'process';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // dotenv
+  config();
+
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enableCors({ credentials: true, origin: true });
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('ejs');
+
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT, () => {
+    console.log(`Main service available on PORT: ${PORT}`);
+  });
 }
 bootstrap();
