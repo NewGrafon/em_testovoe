1642e781929b1d874eafd7f1f837945fde79a751
NewGrafon <ultrafig73@gmail.com>
2023-10-15 19:32:55 +0400

    refactoring

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,7 +1,24 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { parseNumberOrNull } from '../../functions/parse';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
+  constructor(obj: any) {
+    super(false);
+
+    this.currentName = obj.currentName;
+    this.currentSurname = obj.currentSurname;
+    this.currentCity = obj.currentCity;
+    this.currentAge = Number(obj.currentAge);
+
+    this.name =
+      obj.name.trim().length === 0 ? this.currentName : obj.name.trim();
+    this.surname =
+      obj.surname.trim().length === 0 ? this.surname : obj.surname.trim();
+    this.city = obj.city.trim().length === 0 ? this.city : obj.city.trim();
+    this.age = parseNumberOrNull(obj.age) ?? obj.currentAge;
+  }
+
   currentName: string;
   currentSurname: string;
   currentCity: string;
